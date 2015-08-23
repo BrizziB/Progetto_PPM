bd700611ea8ddf00eac53bd1047d4298764059da
Alberto <alby.pitti@gmail.com>
2015-08-23 19:05:58 +0200

    Spostata ListaOggettiVoti in un file

--- Progetto_PPM/app.js
+++ Progetto_PPM/app.js
@@ -9,6 +9,8 @@ var session = require('express-session');
 var HashMap = require('hashmap');
 var mongoose = require('mongoose');
 var FileStore = require('session-file-store')(session);
+//classi personali
+var ListaOggettiVoti=require('./classes/ListaOggettiVoti.js');
 
 //inizializzazione passport e strategie
 var passport = require('passport');
@@ -112,135 +114,6 @@ function contenitoreVoti(nomeOggetto, numeroVoti){
 	};
 }
 
-/*questo oggetto contiene la lista degli oggetti da votare.
- * E' fatta da due mappe hash. Entrambe utilizzano un ID univoco per ciascun oggetto
- * e una contiene l'oggetto, l'altro il numero di voti
- */
-var idGenerator= (function(){
-	var baseID=0;
-	return function(){
-		return baseID++;
-	};
-})();
-
-function listaOggettiVoti(){
-	var id;
-	var contenitoreVoti=new HashMap();
-	var contenitoreOggetti = new HashMap();
-	if (arguments.length!==0){
-		for(var i=0,l=arguments.length;i<l;i++){
-			id=idGenerator();
-			contenitoreOggetti.set(arguments[i],id);
-			contenitoreVoti.set(id,0);
-		}
-	}	
-	this.aggiungiOggetto=function(nomeOggetto,nVoti){
-		if (contenitoreOggetti.has(nomeOggetto)){
-			return false;
-		}
-		if (nVoti===undefined){
-			nVoti=0;
-		}
-		id =idGenerator();
-		contenitoreOggetti.set(nomeOggetto,id);
-		contenitoreVoti.set(id,nVoti);
-		return true;
-	};
-	this.rimuoviOggetto=function(nomeOggetto){
-		if (contenitoreOggetti.has(nomeOggetto)){
-			id = contenitoreOggetti.get(nomeOggetto);
-			contenitoreOggetti.remove(nomeOggetto);
-			contenitoreVoti.remove(id);
-			return true;		
-		}
-		return false;
-	};
-	
-	this.prendiVoti=function(nomeOggetto){
-		if (contenitoreOggetti.has(nomeOggetto)){
-			id = contenitoreOggetti.get(nomeOggetto);
-			return contenitoreVoti.get(id);
-		}
-		return null;
-	};
-
-	this.impostaVoto=function(nomeOggetto,nVoti){
-		if(contenitoreOggetti.has(nomeOggetto)){
-			id=contenitoreOggetti.get(nomeOggetto);
-		}
-		else{
-			id=idGenerator();
-			contenitoreOggetti.set(nomeOggetto,id);
-		}
-		contenitoreVoti.set(id,nVoti);
-	};
-	
-	var incrementaVoto=function(nomeOggetto,nVoti){
-		var sostituto;
-		if(contenitoreOggetti.has(nomeOggetto)){
-			id = contenitoreOggetti.get(nomeOggetto);
-			sostituto= nVoti+contenitoreVoti.get(id);
-		}
-		else{
-			id=idGenerator();
-			sostituto=nVoti;
-			contenitoreOggetti.set(nomeOggetto,nVoti);
-		}
-		contenitoreVoti.set(id,sostituto);
-		//console.log(nomeOggetto);
-	};
-	
-	this.modificaVoto=function(nomeOggetto,nVoti){
-		incrementaVoto(nomeOggetto,nVoti);
-	};
-	this.aggiungiVoto=function(nomeOggetto,nVoti){
-		if (nVoti===undefined){
-			nVoti=1;
-		}
-		else if (nVoti <= 0){
-			return false;
-		}
-		incrementaVoto(nomeOggetto,nVoti);
-		return true;
-	};
-	this.rimuoviVoto=function(nomeOggetto,nVoti){
-		if (nVoti===undefined){
-			nVoti=1;
-		}
-		else if (nVoti <= 0){
-			return false;
-		}
-		incrementaVoto(nomeOggetto,0-nVoti);
-		return true;
-	};
-	this.rimuoviLista = function(){
-		contenitoreOggetti.clear();
-		contenitoreVoti.clear();
-	};
-	
-	this.listaOggetti=function(){
-		return contenitoreOggetti.keys();
-	};
-	this.numeroOggetti=function(){
-		return contenitoreOggetti.count();
-	};
-	this.listaVoti=function(){
-		return contenitoreVoti.clone();
-	};
-	this.oggettoDaID=function(idOggetto){
-		return contenitoreOggetti.search(idOggetto);
-	};
-	this.listaOrdinataPerNome=function(){
-		return contenitoreOggetti.keys().sort();
-	};
-	//doppio ciclo necessario per unire oggetto al voto
-	this.ciclaLista=function(funzione){
-	contenitoreOggetti.forEach(function(id,nomeOggetto){
-			funzione(contenitoreVoti.get(id),nomeOggetto);
-		});
-	};
-	id = null;
-}
 
 function Settings(){
 	var nFoulsBeforePenalty;
@@ -600,7 +473,7 @@ function disabilitaVotoUtente(sess,sockAttuali){
 	}	
 }
 //TODO: i voti devono essere mantenuti dall'admin
-var listaTitoli = new listaOggettiVoti('Test1','Test2','Test3');
+var listaTitoli = new ListaOggettiVoti('Test1','Test2','Test3');
 
 winVote.on('connection', function(socket){
 	// ---- Boris -----
---
+++ Progetto_PPM/classes/ListaOggettiVoti.js
@@ -0,0 +1,133 @@
+var HashMap = require('hashmap');
+
+/*questo oggetto contiene la lista degli oggetti da votare.
+ * E' fatta da due mappe hash. Entrambe utilizzano un ID univoco per ciascun oggetto
+ * e una contiene l'oggetto, l'altro il numero di voti
+ */
+var idGenerator= (function(){
+	var baseID=0;
+	return function(){
+		return baseID++;
+	};
+})();
+
+function ListaOggettiVoti(){
+	var id;
+	var contenitoreVoti=new HashMap();
+	var contenitoreOggetti = new HashMap();
+	if (arguments.length!==0){
+		for(var i=0,l=arguments.length;i<l;i++){
+			id=idGenerator();
+			contenitoreOggetti.set(arguments[i],id);
+			contenitoreVoti.set(id,0);
+		}
+	}	
+	this.aggiungiOggetto=function(nomeOggetto,nVoti){
+		if (contenitoreOggetti.has(nomeOggetto)){
+			return false;
+		}
+		if (nVoti===undefined){
+			nVoti=0;
+		}
+		id =idGenerator();
+		contenitoreOggetti.set(nomeOggetto,id);
+		contenitoreVoti.set(id,nVoti);
+		return true;
+	};
+	this.rimuoviOggetto=function(nomeOggetto){
+		if (contenitoreOggetti.has(nomeOggetto)){
+			id = contenitoreOggetti.get(nomeOggetto);
+			contenitoreOggetti.remove(nomeOggetto);
+			contenitoreVoti.remove(id);
+			return true;		
+		}
+		return false;
+	};
+	
+	this.prendiVoti=function(nomeOggetto){
+		if (contenitoreOggetti.has(nomeOggetto)){
+			id = contenitoreOggetti.get(nomeOggetto);
+			return contenitoreVoti.get(id);
+		}
+		return null;
+	};
+
+	this.impostaVoto=function(nomeOggetto,nVoti){
+		if(contenitoreOggetti.has(nomeOggetto)){
+			id=contenitoreOggetti.get(nomeOggetto);
+		}
+		else{
+			id=idGenerator();
+			contenitoreOggetti.set(nomeOggetto,id);
+		}
+		contenitoreVoti.set(id,nVoti);
+	};
+	
+	var incrementaVoto=function(nomeOggetto,nVoti){
+		var sostituto;
+		if(contenitoreOggetti.has(nomeOggetto)){
+			id = contenitoreOggetti.get(nomeOggetto);
+			sostituto= nVoti+contenitoreVoti.get(id);
+		}
+		else{
+			id=idGenerator();
+			sostituto=nVoti;
+			contenitoreOggetti.set(nomeOggetto,nVoti);
+		}
+		contenitoreVoti.set(id,sostituto);
+		//console.log(nomeOggetto);
+	};
+	
+	this.modificaVoto=function(nomeOggetto,nVoti){
+		incrementaVoto(nomeOggetto,nVoti);
+	};
+	this.aggiungiVoto=function(nomeOggetto,nVoti){
+		if (nVoti===undefined){
+			nVoti=1;
+		}
+		else if (nVoti <= 0){
+			return false;
+		}
+		incrementaVoto(nomeOggetto,nVoti);
+		return true;
+	};
+	this.rimuoviVoto=function(nomeOggetto,nVoti){
+		if (nVoti===undefined){
+			nVoti=1;
+		}
+		else if (nVoti <= 0){
+			return false;
+		}
+		incrementaVoto(nomeOggetto,0-nVoti);
+		return true;
+	};
+	this.rimuoviLista = function(){
+		contenitoreOggetti.clear();
+		contenitoreVoti.clear();
+	};
+	
+	this.listaOggetti=function(){
+		return contenitoreOggetti.keys();
+	};
+	this.numeroOggetti=function(){
+		return contenitoreOggetti.count();
+	};
+	this.listaVoti=function(){
+		return contenitoreVoti.clone();
+	};
+	this.oggettoDaID=function(idOggetto){
+		return contenitoreOggetti.search(idOggetto);
+	};
+	this.listaOrdinataPerNome=function(){
+		return contenitoreOggetti.keys().sort();
+	};
+	//doppio ciclo necessario per unire oggetto al voto
+	this.ciclaLista=function(funzione){
+	contenitoreOggetti.forEach(function(id,nomeOggetto){
+			funzione(contenitoreVoti.get(id),nomeOggetto);
+		});
+	};
+	id = null;
+}
+
+module.exports = ListaOggettiVoti;
