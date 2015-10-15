396e9bb97131e89be8db0ce10f6821bec3192116
Boris <boris.brizzi@gmail.com>
2015-10-15 11:58:31 +0200

    Implementati wait

--- Progetto_PPM/routes/wait.js
+++ Progetto_PPM/routes/wait.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('wait');
+	var admin = req.app.get('admin');  		
+	var titCat;
+	titCat.categoria = admin.getCurrentCategory();
+	switch(req.query.type){
+  	case play: 
+  		res.render('wait');
+  		break;
+  	
+  	case title:
+  		titCat.categoria = null; 	
+
+  	case category:
+  		titCat.titolo = admin.getCurrentTitle();
+  		res.render('waitTitleCategory',titCat);
+		break;
+	
+  	case result: 
+  		res.render('waitResult');
+  		break;
+  	
+  	default:
+  		console.log('Errore: tipo di wait non supportato!');
+  }  
 });
 
 module.exports = router;
